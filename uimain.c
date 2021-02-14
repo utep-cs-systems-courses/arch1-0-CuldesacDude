df81c2fb3db4ac3476ea4e97e3307a709910802f
Kevin Ramirez <krramirez@miners.utep.edu>
2021-02-14 16:09:37 -0700

    Finished objectives for project 0, modified draw.h, draw_chars.c, draw_shapes.c, uimain.c.

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = T, Square = S, Chars = c, Arrow = t) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -16,18 +16,22 @@ int main()
 
     // Given the user answer, select which method to call
     switch (c) {
-    case 't':
+    case 'T':
       puts("You selected triangle:");
       print_triangle(5, 7);
       break;
-    case 's':
+    case 'S':
       puts("You selected square:");
       print_square(5, 5);
       break;
     case 'c':
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
-	print_char_11x16(c);
+	print_char_8x12(c);
+      break;
+    case 't':
+      puts("Printing arrow: ");
+      print_arrow();
       break;
     case 'q':
       puts("Bye!");
